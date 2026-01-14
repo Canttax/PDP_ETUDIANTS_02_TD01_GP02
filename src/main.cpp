e42158d9fee33cd1a878e544af05f1f5284e881b
Canttax <timfrteam0@gmail.com>
2026-01-14 15:55:45 +0100

    Etape 1

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,122 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <WiFi.h>
+#include "WiFiClientSecure.h"
+#include <PubSubClient.h>
 
-// Define the pins that we will use
-#define SENSOR 33
+#define CAPTEUR 33
 #define LED 26
 #define DHTTYPE DHT11
+#define SLEEP_TIME 5
 
-DHT_Unified dht(SENSOR, DHTTYPE);
+const char* wifi_ssid = "iPhone de Sacha";
+const char* wifi_password = "b19fneiuwj";
+const char* mqtt_server = "27cc61dbaffc4da08cd0081cabd8cf01.s2.eu.hivemq.cloud";
+int mqtt_port = 8883;
+const char* mqtt_user = "create_ece";
+const char* mqtt_pass = "create123A";
+const char* client_id = "TD02_GP07";
+
+static const char ca_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+DHT dht(CAPTEUR, DHTTYPE);
+
+WiFiClientSecure client;
+PubSubClient mqtt_client(client);
+
+void connect_wifi() {
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConnected.");
+}
+
 
 void setup() {
-  // Begin serial communication
-  Serial.begin(9600);
-  delay(100);
+  Serial.begin(115200);
+  delay(1000);
 
-  // Connect to WiFi
-  // ...
-  
-  // Configure MQTT server
-  // ...
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
 
-  // Start listening to the DHT11
   dht.begin();
 
-  sensors_event_t event;
+  Serial.println("=== Etape 1 - INTERNNEEEEET (MQTT) ===");
 
-  // Get temperature event and print its value
-  float temp_measure = -999.0;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  } else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-    temp_measure = event.temperature;
+  connect_wifi();
+
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+  client.setCACert(ca_cert);
+  
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  bool ok = mqtt_client.connect(client_id, mqtt_user, mqtt_pass);
+
+  if (ok && !isnan(humidity) && !isnan(temperature)) {
+    String topic_temp = String(client_id) + "/temp";
+    String topic_hum  = String(client_id) + "/relhum";
+
+    char payload_temp[16];
+    char payload_hum[16];
+    dtostrf(temperature, 0, 2, payload_temp);
+    dtostrf(humidity, 0, 2, payload_hum);
+
+    mqtt_client.publish(topic_temp.c_str(), payload_temp, true);
+    mqtt_client.publish(topic_hum.c_str(), payload_hum, true);
+
+    mqtt_client.loop();
+    delay(1000);
+    mqtt_client.loop();
+
+    mqtt_client.disconnect();
+
+
   }
 
-  // Get humidity event and print its value.
-  float relative_humidity_measure = -999.0;
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  } else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-    relative_humidity_measure = event.relative_humidity;
-  }
+  Serial.println("Passage en deep sleep...");
+  Serial.println();
 
-  // Send data to the broker with MQTT
-  // ...
-
-  Serial.println("Going to sleep for 5 seconds...");
-  delay(100);
-  ESP.deepSleep(5e6);
+  esp_sleep_enable_timer_wakeup((uint64_t)SLEEP_TIME * 1000000ULL);
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  // Not needed anymore, the function is kept so Platformio does not complain.
 }
